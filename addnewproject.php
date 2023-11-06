5bd7b8e2c7b0c584ba4392022f4c4f12a7a9e76d
Qal22 <haikal2205hbk@gmail.com>
2023-11-06 17:36:51 +0800

    haha

--- addnewproject.php
+++ addnewproject.php
@@ -3,25 +3,15 @@
   <head>
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta
-      name="viewport"
-      content="width=device-width, initial-scale=1, shrink-to-fit=no"
-    />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
     <meta name="description" content="" />
     <meta name="author" content="" />
 
     <title>Add New Project</title>
 
     <!-- Custom fonts for this template-->
-    <link
-      href="vendor/fontawesome-free/css/all.min.css"
-      rel="stylesheet"
-      type="text/css"
-    />
-    <link
-      href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
-      rel="stylesheet"
-    />
+    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css"/>
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet"/>
 
     <!-- Custom styles for this template-->
     <link href="css/sb-admin-2.min.css" rel="stylesheet" />
@@ -31,22 +21,12 @@
     <!-- Page Wrapper -->
     <div id="wrapper">
       <!-- Sidebar -->
-      <ul
-        class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion"
-        id="accordionSidebar"
-      >
+      <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
         <!-- Sidebar - Brand -->
-        <a
-          class="sidebar-brand d-flex align-items-center justify-content-center"
-          href="index.html"
-        >
+        <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
           <div class="sidebar-brand-icon">
             <!--<i class="fas fa-laugh-wink"></i>-->
-            <img
-              src="img/logoanamuslim.png"
-              alt="Your Logo"
-              style="height: 44px; width: 64px"
-            />
+            <img src="img/logoanamuslim.png" alt="Your Logo" style="height: 44px; width: 64px"/>
           </div>
           <div class="sidebar-brand-text mx-3">Production Tracking</div>
         </a>
@@ -75,23 +55,11 @@
         <!--Change data-target, aria-controls, id, aria-labelledby into other name than any nav-item-->
         <!-- Nav Item 1 - Pages Collapse Menu -->
         <li class="nav-item">
-          <a
-            class="nav-link collapsed"
-            href="#"
-            data-toggle="collapse"
-            data-target="#collapseprepress"
-            aria-expanded="true"
-            aria-controls="collapseprepress"
-          >
+          <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseprepress" aria-expanded="true" aria-controls="collapseprepress">
             <i class="fas fa-fw fa-cog"></i>
             <span>Pre-press</span>
           </a>
-          <div
-            id="collapseprepress"
-            class="collapse"
-            aria-labelledby="headingprepress"
-            data-parent="#accordionSidebar"
-          >
+          <div id="collapseprepress" class="collapse" aria-labelledby="headingprepress" data-parent="#accordionSidebar">
             <div class="bg-white py-2 collapse-inner rounded">
               <h6 class="collapse-header">Sub-process:</h6>
               <a class="collapse-item" href="#">Design & Layout</a>
